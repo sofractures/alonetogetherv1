709bd92b9127339bc4a20f507890b263c88c2f39
sofractures <soundoffractures@gmail.com>
2025-11-25 14:13:11 +0000

    Increase grain opacity to 20% for more visible effect

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       >
         <BackgroundAudio />
         {children}
-        <AnimatedGrainOptimized opacity={12} fps={24} blendMode="overlay" />
+        <AnimatedGrainOptimized opacity={20} fps={24} blendMode="overlay" />
       </body>
     </html>
   );
